81607739f43dad84fa1da646e91dc42ddc555dff
markozh <marlenkozhabekov@gmail.com>
2024-08-10 22:54:36 +0500

    Создание основы программы

---
+++ ChartExamples.py
@@ -0,0 +1,13 @@
+# Примеры построения графиков
+
+import tkinter as tk
+
+# Создание главного окна
+window = tk.Tk()
+window.geometry("450x450")
+window.title("Примры построения графиков")
+
+
+# Запуск цикла mainloop
+window.mainloop()
+
